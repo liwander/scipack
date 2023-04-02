0523a747a52b01011675d7390602b603c480bc2b
anver <whitenuclearrhino@gmail.com>
2023-04-02 11:55:02 +0300

    checkerboard task was submitted and approved

--- checkerboard.m
+++ checkerboard.m
@@ -1,20 +1,19 @@
 function res = checkerboard (rows_number, columns_number)
 
     function board = create_checkerboard (rows_number, columns_number)
-        board = zeros(rows_number, columns_number);
-        board(1:2:rows_number, 1:2:columns_number) = 1
-        board(2:2:rows_number, 2:2:columns_number) = 1
+        board = false(rows_number, columns_number)
+        board(1:2:rows_number, 1:2:columns_number) = true
+        board(2:2:rows_number, 2:2:columns_number) = true
     endfunction
-
 
     switch (nargin)
         case 1
-            res = create_checkerboard(rows_number, rows_number);
+            res = create_checkerboard(rows_number, rows_number)
         case 2 
             res = create_checkerboard(rows_number, columns_number)
         otherwise 
-            printf("Arguments number does not match function description\n");
-            res = [];
+            printf("Arguments number does not match function description\n")
+            res = []
     endswitch
 
 endfunction
