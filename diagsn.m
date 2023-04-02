f9d5f496c47b96dbe7289c10d699a5c75ec9a56b
anver <whitenuclearrhino@gmail.com>
2023-04-02 19:52:57 +0300

    Correct solution of diagsn task

--- diagsn.m
+++ diagsn.m
@@ -2,7 +2,7 @@ function res = diagsn (rows_number, columns_number)
     
     function res = snake_matrix (rows_number, columns_number)
         res = uint32(zeros(rows_number, columns_number))
-        mx_ln = rows_number * columns_number
+        mx_snk_ln = rows_number * columns_number
         mx_diag_ln = min(rows_number, columns_number)
         
         snk_ln = 0
@@ -12,7 +12,7 @@ function res = diagsn (rows_number, columns_number)
         is_top = true
 
         diags = [1:mx_diag_ln - 1]
-        diags = [diags, mx_diag_ln(ones(1, (mx_ln - 2 * sum(diags)) / 3 ))]
+        diags = [diags, mx_diag_ln(ones(1, (mx_snk_ln - 2 * sum(diags)) / mx_diag_ln ))]
         diags = [diags, fliplr(1:mx_diag_ln - 1)]      
         
         for dg_num = 1:columns(diags)
@@ -21,7 +21,6 @@ function res = diagsn (rows_number, columns_number)
                 for m = 1:(diags(dg_num) - 1)
                     res(++ii, --ij) += ++snk_ln
                 endfor
-                is_top = !is_top
                 if (ii < rows_number) 
                     ii++
                 else
@@ -33,13 +32,13 @@ function res = diagsn (rows_number, columns_number)
                 for m = 1:(diags(dg_num) - 1)
                     res(--ii, ++ij) += ++snk_ln
                 endfor
-                is_top = !is_top
-                if (ij < rows_number) 
+                if (ij < columns_number) 
                     ij++
                 else
                     ii++
                 endif
             endif
+            is_top = !is_top
         endfor
 
     endfunction
@@ -50,6 +49,7 @@ function res = diagsn (rows_number, columns_number)
         case 2
             res = snake_matrix(rows_number, columns_number)
         otherwise 
+            res = []
             disp("mismatching arguments number")
     endswitch 
 
